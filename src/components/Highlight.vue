88ba37bc52bf14f5c0eab14226d6de9fecc1a981
Abdulbaki Dursun <eggsydev@gmail.com>
2022-07-22 03:15:52 +0300

    feat: create highlight component with syntax highlighting

---
+++ src/components/Highlight.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+const props = defineProps({
+  code: {
+    type: String,
+    required: false,
+    default: "",
+  },
+});
+
+const getHighlightedJson = computed(() => {
+  let json = props.code
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
+
+  return json.replace(
+    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+    function (match) {
+      let cls = "number";
+      if (/^"/.test(match)) cls = /:$/.test(match) ? "key" : "string";
+      else if (/true|false/.test(match)) cls = "boolean";
+      else if (/null/.test(match)) cls = "null";
+
+      return '<span class="' + cls + '">' + match + "</span>";
+    }
+  );
+});
+</script>
+
+<template>
+  <pre
+    v-html="getHighlightedJson"
+    class="w-full p-4 overflow-x-hidden overflow-y-auto text-sm rounded-lg resize-none no-scrollbar bg-brand/40 h-96"
+  />
+</template>
+
+<style scoped>
+.string {
+  color: #98c379;
+}
+.number {
+  color: #d19a66;
+}
+.boolean {
+  color: #c678dd;
+}
+.null {
+  color: #61afef;
+}
+.key {
+  color: #e06c75;
+}
+</style>
