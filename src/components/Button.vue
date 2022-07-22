9f936dc750870d87dcfd0643381b11d6e4b32743
Abdulbaki Dursun <eggsydev@gmail.com>
2022-07-22 21:52:05 +0300

    style: rounded button if no label

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -30,7 +30,11 @@ defineProps({
     :href="href || null"
     :target="href && blank ? '_blank' : null"
     :rel="href && blank ? 'noopener noreferrer' : null"
-    class="flex items-center justify-center w-full px-8 py-3 space-x-2 text-center transition-colors rounded-lg cursor-pointer lg:w-max bg-brand/40 hover:bg-brand/50"
+    class="flex items-center justify-center w-full text-center transition-colors rounded-lg cursor-pointer lg:w-max bg-brand/40 hover:bg-brand/50"
+    :class="{
+      'rounded-full p-3': !label && icon,
+      'px-8 py-3 space-x-2': label,
+    }"
   >
     <Component :is="icon" />
     <span class="text-sm font-bold">{{ label }}</span>
