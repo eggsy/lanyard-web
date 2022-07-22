e00a75d6a4b2778bb493ac674e4da699bcf63f33
Abdulbaki Dursun <eggsydev@gmail.com>
2022-07-22 03:15:15 +0300

    chore: motion plugin only in client

--- src/plugins/VueUse.client.ts
+++ src/plugins/VueUse.client.ts
@@ -2,6 +2,5 @@ import { defineNuxtPlugin } from "#app";
 import { MotionPlugin } from "@vueuse/motion";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  if (!process.client) return;
   nuxtApp.vueApp.use(MotionPlugin);
 });
